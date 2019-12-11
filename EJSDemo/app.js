10685a66ae4311f97f04f64b28f5847e3f3bedce
Navishka Kularathna <navishka.randika@gmail.com>
2019-12-11 22:48:45 +0530

    posts route added

--- EJSDemo/app.js
+++ EJSDemo/app.js
@@ -5,9 +5,19 @@ app.get("/",function(req,res){
     res.render("home.ejs");
 });
 
-app.get("/love/:thing", function(){
+app.get("/love/:thing", function(req,res){
     var thing = req.params.thing;
     res.render("love.ejs",{thingVar: thing});
+});
+
+app.get("/posts", function(req,res){
+    var posts = [
+        {title:"First post", author: "Susy"},
+        {title:"Pet bunnies", author: "Charlie"},
+        {title:"About Pomskies", author: "Colt"}
+    ];
+
+    res.render("posts.ejs", {posts: posts});
 });
 
 app.listen(3000, function(){
